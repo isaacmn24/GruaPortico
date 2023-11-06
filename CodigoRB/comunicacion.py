3ae98eb2a76d1fd8d41bb4bd7a4d62d933f6feda
Isaac <isaacfelipe2007@gmail.com>
2023-11-06 16:10:59 -0600

    Agregué archivo Comunicacion para las funciones de transmisión de datos de la raspberry pi al STM32

---
+++ CodigoRB/comunicacion.py
@@ -0,0 +1,42 @@
+import serial 
+
+ser = serial.Serial("/dev/ttyACM0", 9600)   # Serial port on Raspberry Pi
+
+def recibirColores():
+    """
+    Descripción
+    -------
+    Para que esta función se concrete tendrá que pasar por cada
+    uno de los 25 espacios y recibir un dato del STM32, si no recibe
+    nada, no avanzará y se quedará enfrascada esperando por siempre.
+    
+    Returns
+    -------
+    Retorna la matriz escaneada por el STM32
+
+    """
+    matriz_escaneada = [[0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0],
+                        [0, 0, 0, 0, 0]]
+    for fila in matriz_escaneada:
+        for columna in fila:
+            if ser.in_waiting() > 0:
+                color = ser.read(1)
+                matriz_escaneada[fila][columna] = color
+    return matriz_escaneada
+
+def enviarSTM32(dato):
+    """
+    Parametros
+    ----------
+    dato : BYTE
+        Dato a enviar al STM32.
+
+    Returns
+    -------
+    Retorna un 1 si se envió satisfactoriamente el dato.
+
+    """
+    return ser.write(dato)
