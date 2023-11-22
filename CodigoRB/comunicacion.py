3c7d3d447b4593296bc65ae64a67ed1c17132653
Isaac <isaacfelipe2007@gmail.com>
2023-11-22 17:45:23 -0600

    Agregué código en CodigoRB para comunicar la raspberry pi con el stm32.

--- CodigoRB/comunicacion.py
+++ CodigoRB/comunicacion.py
@@ -22,9 +22,10 @@ def recibirColores():
                         [0, 0, 0, 0, 0]]
     for fila in matriz_escaneada:
         for columna in fila:
-            if ser.in_waiting() > 0:
-                color = ser.read(1)
-                matriz_escaneada[fila][columna] = color
+            while ser.in_waiting <= 0:
+                if ser.in_waiting() > 0:
+                    color = ser.read(1)
+                    matriz_escaneada[fila][columna] = color
     return matriz_escaneada
 
 def enviarSTM32(dato):
@@ -40,3 +41,11 @@ def enviarSTM32(dato):
 
     """
     return ser.write(dato)
+
+def recibirSTM32():
+    """
+    Retorna el byte leído de la STM32
+    """
+    while ser.in_waiting <= 0:
+        if ser.in_waiting() > 0:
+            return ser.read(1)
