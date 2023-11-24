b2cd58579ed1b13f91e9f87c507b938b9298f7d4
Isaac <isaacfelipe2007@gmail.com>
2023-11-24 09:54:50 -0600

    Agregué código y archivo de texto con calibración de los sensores.

--- CodigoSTM32/calibracionSensores/calibracionSensores.ino
+++ CodigoSTM32/calibracionSensores/calibracionSensores.ino
@@ -22,9 +22,11 @@
 int frecuenciaRojo = 0;
 int frecuenciaVerde = 0;
 int frecuenciaAzul = 0;
-int colorRojo;
-int colorVerde;
-int colorAzul;
+int colorRojo = 0;
+int colorVerde = 0;
+int colorAzul = 0;
+double distancia;
+int promedio = 5000;
 
 void setup() {
   // Definiendo las Salidas
@@ -43,57 +45,82 @@ void setup() {
 
 void loop()
 {
-  // Definiendo la lectura de los fotodiodos con filtro rojo
-  digitalWrite(sensorColor_S2,LOW);
-  digitalWrite(sensorColor_S3,LOW);
-  
-  // Leyendo la frecuencia de salida del sensor
-  frecuenciaRojo = pulseIn(sensorColor_salida, LOW);
-  
-  // Mostrando por el monitor Serial el valor para el rojo (R = Red)
-  Serial.print("R = ");
-  Serial.print(frecuenciaRojo);
-  delay(100);
-  
-  // Definiendo la lectura de los fotodiodos con filtro verde
-  digitalWrite(sensorColor_S2,HIGH);
-  digitalWrite(sensorColor_S3,HIGH);
-  
-  // Leyendo la frecuencia de salida del sensor
-  frecuenciaVerde = pulseIn(sensorColor_salida, LOW);
+  for(int contador = 0; contador <= promedio-1; contador++) {
+    // Definiendo la lectura de los fotodiodos con filtro rojo
+    digitalWrite(sensorColor_S2,LOW);
+    digitalWrite(sensorColor_S3,LOW);
+    
+    // Leyendo la frecuencia de salida del sensor
+    frecuenciaRojo = pulseIn(sensorColor_salida, LOW);
 
-  // Mostrando por el monitor Serial el valor para el verde (G = Green)
-  Serial.print(" G = ");
-  Serial.print(frecuenciaVerde);
-  delay(100);
- 
-  // Definiendo la lectura de los fotodiodos con filtro azul
-  digitalWrite(sensorColor_S2,LOW);
-  digitalWrite(sensorColor_S3,HIGH);
-  
-  // Leyendo la frecuencia de salida del sensor
-  frecuenciaAzul = pulseIn(sensorColor_salida, LOW);
-  
-  // Mostrando por el monitor Serial el valor para el azul (B = Blue)
-  Serial.print(" B = ");
-  Serial.println(frecuenciaAzul);
-  delay(100);
+    // Sumo las frecuencias para sacar promedio luego
+    colorRojo = colorRojo + frecuenciaRojo;
 
-  // Comprobar cual es el color detectado y mostrarlo
-  // esta parte del código realmente va en la RASP
-  // int color;
-  // if(colorRojo > colorVerde && colorRojo > colorAzul){        // Color rojo
-  //   color = 1;
-  // }
-  // else if(colorVerde > colorRojo && colorVerde > colorAzul){  // Color verde
-  //   color = 3;
-  // }
-  // else if(colorAzul > colorRojo && colorAzul > colorVerde){   // Color azul
-  //   color = 2;
-  // }
-  // else {                                                      // Espacio vacío
-  //   color = 4;
-  // }
+    // Mostrando por el monitor Serial el valor para el rojo (R = Red)
+    //Serial.print("R = ");
+    //Serial.print(frecuenciaRojo);
+    
+    // Definiendo la lectura de los fotodiodos con filtro verde
+    digitalWrite(sensorColor_S2,HIGH);
+    digitalWrite(sensorColor_S3,HIGH);
+    
+    // Leyendo la frecuencia de salida del sensor
+    frecuenciaVerde = pulseIn(sensorColor_salida, LOW);
+
+    colorVerde = colorVerde + frecuenciaVerde;
+
+    // Mostrando por el monitor Serial el valor para el verde (G = Green)
+    //Serial.print(" G = ");
+    //Serial.print(frecuenciaVerde);
+  
+    // Definiendo la lectura de los fotodiodos con filtro azul
+    digitalWrite(sensorColor_S2,LOW);
+    digitalWrite(sensorColor_S3,HIGH);
+    
+    // Leyendo la frecuencia de salida del sensor
+    frecuenciaAzul = pulseIn(sensorColor_salida, LOW);
+
+    colorAzul = colorAzul + frecuenciaAzul;
+    
+    // Mostrando por el monitor Serial el valor para el azul (B = Blue)
+    //Serial.print(" B = ");
+    //Serial.println(frecuenciaAzul);
+  }
+
+  colorRojo = colorRojo/promedio;
+  colorVerde = colorVerde/promedio;
+  colorAzul = colorAzul/promedio;
+
+  distancia = HCSR04.measureDistanceCm()[0];
+  Serial.print("Distancia: "); Serial.println(distancia);
+
+  // Serial.print(" R = ");
+  // Serial.print(colorRojo);
+  // Serial.print(" G = ");
+  // Serial.print(colorVerde);
+  // Serial.print(" B = ");
+  // Serial.println(colorAzul);
+
+  Serial.print("Color: ");
+  if(colorVerde < 6 && colorRojo < 6 && colorAzul < 6){        // Color blanco                                                   // Espacio vacío
+    Serial.println(4);
+  }
+  else if(colorRojo < colorVerde && colorRojo < colorAzul){    // Color rojo
+    Serial.println(1);
+  }
+  else if(colorAzul < colorRojo && colorAzul < colorVerde){    // Color azul
+    Serial.println(2);
+  }
+  else {                                                       // Color verde
+    Serial.println(3);
+  }
+
+  colorRojo = 0;
+  colorVerde = 0;
+  colorAzul = 0;
+  distancia = 0;
+
+  //delay(5000);
 }
 
 bool alarmaDistancia()
